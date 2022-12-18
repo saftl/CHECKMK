4086f738749180fac77c0592087a03a7b3d21941
saftl <54141429+saftl@users.noreply.github.com>
2022-12-18 13:11:35 +0100

    IP as variable

--- json.sh
+++ json.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 jsonip=192.168.0.111
-jsonraw=$(curl http://192.168.0.111/cm?cmnd=status%200 -s -H "Accept: application/json")
+jsonraw=$(curl http://${jsonip}/cm?cmnd=status%200 -s -H "Accept: application/json")
 echo $jsonip
 echo $jsonraw | jq --raw-output '.StatusSNS.ENERGY.Power'
 echo $jsonraw | jq --raw-output '.StatusSNS.ENERGY.Yesterday'
