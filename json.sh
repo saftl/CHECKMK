fd9a2c0777f3ffcc26f46fcdde2c40238c54ca95
saftl <54141429+saftl@users.noreply.github.com>
2022-12-18 12:59:50 +0100

    v0.0.2

--- json.sh
+++ json.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
-json_data='{"key":"value"}'
-echo $json_data | jq --raw-output '.key'
+jsonip=192.168.0.111
+jsonraw=$(curl http://192.168.0.111/cm?cmnd=status%200 -s -H "Accept: application/json")
+echo $jsonip
+echo $jsonraw | jq --raw-output '.StatusSNS.ENERGY.Power'
+echo $jsonraw | jq --raw-output '.StatusSNS.ENERGY.Yesterday'
